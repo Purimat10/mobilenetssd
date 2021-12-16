52554d5d84e5009e2cf6a15357f0c976dfadd37c
Purimat10 <90917855+Purimat10@users.noreply.github.com>
2021-12-16 11:45:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def event_handle(event,json_line):
         elif (msg == "กินข้าวกันไหม") :
             replyObj = TextSendMessage(text="ไม่หิวค่ะ")
             line_bot_api.reply_message(rtoken, replyObj)
-         elif msg == "covid" :
+        elif msg == "covid" :
             url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
             response = requests.get(url)
             response = response.json()
